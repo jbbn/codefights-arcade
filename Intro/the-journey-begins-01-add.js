fdbe884cf4a4aee058bf39a0caaf00597d8f866d
jbbn <joaobbn@gmail.com>
2018-06-15 08:52:07 -0300

    The Journey Begins

---
+++ Intro/the-journey-begins-01-add.js
@@ -0,0 +1,14 @@
+/**
+ * Retorna a soma de dois números
+ * @param {number} a - Um número inteiro
+ * @param {number} b - Um número inteiro
+ * @return {number} - A soma dos números fornecidos
+*/
+const add = (a, b) =>  a + b
+
+{
+  // Forma alternativa utilizando ES5
+  function add (a, b) {
+    return a + b
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# codefights-arcade
+# CodeFights Arcade
+
+A intenção deste repositório é apenas criar uma referência de conhecimento, registrando abordagens para resolver os problemas propostos no CodeFights Arcade Universe; utilize por sua conta e risco.
